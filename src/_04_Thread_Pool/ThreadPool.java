0694792361009ef8af4ce89293fc4ab83c05cabe
league <league@imac-12.attlocal.net>
2020-03-01 17:21:27 -0800

    Finished

--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,36 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+	Thread[] threads;
+	ConcurrentLinkedQueue<Task> taskQueue;
+	public ThreadPool(int number) {
+		// TODO Auto-generated constructor stub
+		threads = new Thread[number];
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		for (int i = 0; i < number; i++) {
+			threads[i] = new Thread(new Worker(taskQueue));
+		}
+	}
+	public void addTask(Task a) {
+		taskQueue.add(a);
+	}
+	public void start() {
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+		for(int i = 0; i <threads.length; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	
 
 }
--- src/_04_Thread_Pool/Worker.java
+++ src/_04_Thread_Pool/Worker.java
@@ -1,12 +1,20 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Worker implements Runnable {
 
-	@Override
+	ConcurrentLinkedQueue<Task> taskQueue;
+	
+	Worker(ConcurrentLinkedQueue<Task> taskQueue){
+		this.taskQueue = taskQueue;
+	}
+	
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(taskQueue.size() != 0) {
+			taskQueue.poll().perform();
+		}
 	}
 
 
